423b4c675b4aaa6656f7552d7782941868607f13
scarger <supremeterminal@gmail.com>
2020-08-04 23:08:20 -0400

    remove dist

--- dist/config/keys.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    google: {
-        clientID: '939055781768-sh1mm0jhkclsmo7jii2qtqrliapadtg4.apps.googleusercontent.com',
-        clientSecret: '4y5WofDyXy6qOkzqs7qW6aWY'
-    }
-};
-//# sourceMappingURL=keys.js.map
